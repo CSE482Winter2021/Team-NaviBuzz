9af35929b4c9d316b1d581a9c6036e37e955ae41
Dylan Burton <burtond@uw.edu>
2021-02-14 14:47:23 -0700

    fix setup

--- android-app-helloworld-master/app/src/main/java/com/navisens/demo/android_app_helloworld/MainActivity.java
+++ android-app-helloworld-master/app/src/main/java/com/navisens/demo/android_app_helloworld/MainActivity.java
@@ -1,9 +1,11 @@
 package com.navisens.demo.android_app_helloworld;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.core.app.ActivityCompat;
 
 import com.navisens.motiondnaapi.MotionDna;
@@ -11,7 +13,9 @@ import com.navisens.motiondnaapi.MotionDnaSDK;
 import com.navisens.motiondnaapi.MotionDnaSDKListener;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 
 /*
  * For complete documentation on the MotionDnaSDK API
@@ -22,14 +26,16 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity implements MotionDnaSDKListener {
 
     MotionDnaSDK motionDnaSDK;
-    TextView textView;
+    TextView receiveMotionDnaTextView;
+    TextView reportStatusTextView;
     private static final int REQUEST_MDNA_PERMISSIONS=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.HELLO);
+        receiveMotionDnaTextView = findViewById(R.id.receiveMotionDnaTextView);
+        reportStatusTextView = findViewById(R.id.reportStatusTextView);
         // Requests app
         ActivityCompat.requestPermissions(this,MotionDnaSDK.getRequiredPermissions()
                 , REQUEST_MDNA_PERMISSIONS);
@@ -47,7 +53,7 @@ public class MainActivity extends AppCompatActivity implements MotionDnaSDKListe
         String devKey = "<--DEVELOPER-KEY-HERE-->";
 
         motionDnaSDK = new MotionDnaSDK(this.getApplicationContext(),this);
-
+        motionDnaSDK.startForegroundService();
         //    This functions starts up the SDK. You must pass in a valid developer's key in order for
         //    the SDK to function. IF the key has expired or there are other errors, you may receive
         //    those errors through the reportError() callback route.
@@ -64,39 +70,44 @@ public class MainActivity extends AppCompatActivity implements MotionDnaSDKListe
         str += MotionDnaSDK.SDKVersion() + "\n";
         str += "Lat: " + motionDna.getLocation().global.latitude + " Lon: " + motionDna.getLocation().global.longitude + "\n";
         MotionDna.CartesianLocation location = motionDna.getLocation().cartesian;
-        str += String.format(" (%.2f, %.2f, %.2f)\n",location.x, location.y, location.z);
+        str += String.format(Locale.US," (%.2f, %.2f, %.2f)\n",location.x, location.y, location.z);
         str += "Hdg: " + motionDna.getLocation().global.heading +  " \n";
-        str += "motionType: " + motionDna.getClassifiers().get("motion").prediction.label + "\n";
+        str += "motionType: " + Objects.requireNonNull(motionDna.getClassifiers().get("motion")).prediction.label + "\n";
 
         str += "Predictions (BETA): \n\n";
         HashMap<String, MotionDna.Classifier> classifiers =  motionDna.getClassifiers();
         for (Map.Entry<String, MotionDna.Classifier> entry : classifiers.entrySet()) {
             str += String.format("Classifier: %s\n",entry.getKey());
-            str += String.format("\tcurrent prediction: %s confidence: %.2f\n",entry.getValue().prediction.label, entry.getValue().prediction.confidence);
+            str += String.format(Locale.US,"\tcurrent prediction: %s confidence: %.2f\n",entry.getValue().prediction.label, entry.getValue().prediction.confidence);
             str += "\tprediction stats:\n";
 
             for (Map.Entry<String, MotionDna.PredictionStats> statsEntry : entry.getValue().statistics.entrySet()) {
-                str += String.format("\t%s",statsEntry.getKey());
-                str += String.format("\t duration: %.2f\n",statsEntry.getValue().duration);
-                str += String.format("\t distance: %.2f\n",statsEntry.getValue().distance);
+                str += String.format(Locale.US,"\t%s",statsEntry.getKey());
+                str += String.format(Locale.US,"\t duration: %.2f\n",statsEntry.getValue().duration);
+                str += String.format(Locale.US,"\t distance: %.2f\n",statsEntry.getValue().distance);
             }
             str += "\n";
         }
 
-        textView.setTextColor(Color.BLACK);
 
         final String fstr = str;
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                textView.setText(fstr);
+                receiveMotionDnaTextView.setText(fstr);
             }
         });
     }
 
 
     @Override
-    public void reportStatus(MotionDnaSDK.Status status, String s) {
+    public void reportStatus(final MotionDnaSDK.Status status, final String s) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                reportStatusTextView.append(String.format(Locale.US,"Status: %s Message: %s\n",status.toString(),s));
+            }
+        });
         switch (status) {
             case AuthenticationFailure:
                 System.out.println("Error: Authentication Failed " + s);
