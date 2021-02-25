fec721fd3314862b2e41fdcaf1d5cf801483f9d7
Allyson Ely <elyall17@uw.edu>
2021-02-25 11:54:11 -0800

    added database entities and daos

--- android-app-helloworld-master/app/src/main/java/com/navisens/demo/android_app_helloworld/database_obj/Path.java
+++ android-app-helloworld-master/app/src/main/java/com/navisens/demo/android_app_helloworld/database_obj/Path.java
@@ -1,12 +1,17 @@
 package com.navisens.demo.android_app_helloworld.database_obj;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Index;
+import androidx.room.PrimaryKey;
+
 @Entity(indices = {@Index(value = {"name"},
-        unique = true)})
+        unique = true)}, tableName = "paths")
 public class Path {
-    @PrimaryKey
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "path_id")
     public int pid;
 
     @ColumnInfo(name = "name")
-    public string name;
-
+    public String name;
 }
---
+++ android-app-helloworld-master/app/src/main/java/com/navisens/demo/android_app_helloworld/database_obj/PathDao.java
@@ -0,0 +1,28 @@
+package com.navisens.demo.android_app_helloworld.database_obj;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface PathDao {
+
+    @Query("SELECT * FROM paths")
+    public List<Path> getAll();
+
+    @Query("SELECT * FROM paths WHERE name = :name")
+    public Path findByName(String name);
+
+    @Insert
+    public void insertPath(Path p);
+
+    @Delete
+    public void deletePath(Path p);
+
+    @Query("DELETE FROM paths")
+    public void deleteAll();
+
+}
