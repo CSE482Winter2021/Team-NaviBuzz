d65b5f71a55571312b9beadf21ac3b820222aab0
Dylan Burton <burtond@uw.edu>
2021-03-01 20:07:52 -0700

    cleanup

--- android-app-helloworld-master/app/src/main/java/com/navisens/demo/android_app_helloworld/RecordPathActivity.java
+++ android-app-helloworld-master/app/src/main/java/com/navisens/demo/android_app_helloworld/RecordPathActivity.java
@@ -9,6 +9,7 @@ import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
